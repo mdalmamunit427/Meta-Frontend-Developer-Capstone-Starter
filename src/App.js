f03ec120cf872464ad00bdb55661ee013613adb6
sdjean88 <125686595+sdjean88@users.noreply.github.com>
2023-08-20 22:18:22 -0400

    created header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
   return (
 
     <>
     <Nav />
+    <Header />
     </>
   );
 }
